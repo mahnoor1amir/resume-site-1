352c1c2218e6fadf5b7fdc4b88690a4029e60064
Carolyn <ckaminski@albany.edu>
2020-04-18 01:46:15 -0400

    local image working

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,8 +2,12 @@ import React from "react"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `teal` }}>
-    <Header headerText="Contact" />
-    <p>Send us a message!</p>
+  <div>
+    <Header headerText="Contact"/>   
+
+    <h1>I'd love to talk! Email me at the address below</h1>
+    <p>
+      <a href="mailto:me@example.com">me@example.com</a>
+    </p>
   </div>
 )
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,6 +2,8 @@ import React from "react"
 import Header from "../components/header"
 import Container from "../components/container"
 
+import phone from "../images/phone-sm.jpg" 
+
 export default () => (
   <div style={{ color: `teal` }}>
   	<Header headerText="Projects"/>   
@@ -9,24 +11,29 @@ export default () => (
 	<h2>Projects</h2>
 
 	<Container>
-		<img src="https://source.unsplash.com/random/400x200" alt="" />
+		<img src={phone} alt="alt" />
 
 	    <h1>A Great Project</h1>
 	    <p>CSS Modules are cool</p>
 	</Container>
 	<Container>
-		<img src="https://source.unsplash.com/random/400x200" alt="" />
+		<img src="https://source.unsplash.com/random/400x200" alt="alt" />
 
 	    <h1>Another Super Amazing Project</h1>
 	    <p>CSS Modules are cool</p>
 	</Container>  
 	<Container>
-		<img src="https://source.unsplash.com/random/400x200" alt="" />
+		<img src="https://source.unsplash.com/random/400x200" alt="alt" />
 
 	    <h1>Showstopping Project</h1>
 	    <p>CSS Modules are cool</p>
 	</Container>
+	<Container>
+		<img src="https://source.unsplash.com/random/400x200" alt="alt" />
 
+	    <h1>Another Showstopping Project</h1>
+	    <p>CSS Modules are cool</p>
+	</Container>
 
   </div>
 
